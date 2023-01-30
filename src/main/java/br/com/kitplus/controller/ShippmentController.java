d55b0e5fbff24690da2e0268cbdee56fe4153747
GustavoPinheiro170 <gustavocampos170@hotmail.com>
2023-01-30 00:26:14 -0300

    Voltando properties

--- src/main/java/br/com/kitplus/controller/ShippmentController.java
+++ src/main/java/br/com/kitplus/controller/ShippmentController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ import br.com.kitplus.service.ShippmentService;
 
 @RestController
 @RequestMapping("/api/v1/shippment")
+@CrossOrigin(origins = {"https://kitplus.com.br/"})
 public class ShippmentController {
     @Autowired
     private ShippmentService shippmentService;
